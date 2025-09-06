07d9f03e07843b82214c32248485c5aaad2a800a
YO <YO@DESKTOP-M7M64B2>
2025-09-05 22:24:54 -0500

    Add:
    - Java_data_types.java
    
    Delete:
    
    - Main.java
    - Module-info.java

---
+++ Java_001/src/main/Java_data_types.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Java_data_types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		byte entero_1 = 2;
+		short entero_2 = 3;
+		int entero_3 = 1;
+		long entero_4 = 2;
+		
+		System.out.println("byte: " + entero_1);
+		System.out.println("short: " + entero_2);
+		System.out.println("int: " + entero_3);
+		System.out.println("long: " + entero_4);
+		
+		double puntoFlotante_1 = 1.1;
+
+	}
+
+}
